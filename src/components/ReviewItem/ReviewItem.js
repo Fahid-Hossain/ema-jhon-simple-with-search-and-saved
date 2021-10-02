069bb9a472ccdbcddff6f2981c63099892bb7190
Fahid Hossain <fahidhossain2021@gmail.com>
2021-10-02 20:31:21 +0600

    quantity add in reviewItem

--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ReviewItem = (props) => {
     // console.log(props.product);
-    const {name,price,key}=props.product;
+    const {name,price,key,quantity}=props.product;
 
     // click handler 
     const removeHandler= props.removeHandler;
@@ -11,6 +11,7 @@ const ReviewItem = (props) => {
         <div className="product">
           <div>
           <h3 className="product-name">{name}</h3>
+          <h5>Quantity: {quantity}</h5>
             <h4>Price: {price}</h4>
             <button onClick={()=>removeHandler(key)} className="btn-regular">Remove</button>
           </div>
