d49d88f0a98084a13d47fc8fa10d7c0e87da84dc
Fahid Hossain <fahidhossain2021@gmail.com>
2021-10-02 20:27:23 +0600

    remove clicked item from localStorage

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import useCart from '../../hooks/useCart/useCart';
 import useProducts from '../../hooks/useProducts/useProducts';
+import { removeFromDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import ReviewItem from '../ReviewItem/ReviewItem';
 
 const OrderReview = () => {
     // useState for all products
-    const [products, setProducts] = useProducts();
+    const [products] = useProducts();
     // useState for Cart 
     const [cart, setCart] = useCart(products);
 
        // onclick handler for remove specific item 
        const removeHandler =(key)=> {
             // console.log("clicked");
+            // remove clicked item from ui
             const newItem = cart.filter(product=>product.key !== key);
             setCart(newItem);
+
+            // remove clicked item from localStorage
+            removeFromDb(key);
        }
     return (
         <div className="shop-container">
